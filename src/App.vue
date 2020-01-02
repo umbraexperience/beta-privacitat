5bd704e8c9459b515b47b0569b682f317006d4c3
Adrix2238 <2238adrix@gmail.com>
2020-01-02 19:54:13 +0100

    Responsive acabat (revisió)

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,21 @@
           momento de utilizar su sitio web. Este proyecto está comprometido con
           la seguridad de los datos de sus usuarios. Cuando le pedimos llenar
           los campos de información personal con la cual usted pueda ser
-          identificado, lo hacemos asegurando que sólo se empleará de acuerdo
+          identificado, lo hacemos asegurando que solo se empleará de acuerdo
           con los siguientes términos.
         </p>
         <p>
           Nuestro sitio web podrá recoger información personal, por ejemplo:
-          Nombre, situación geográfica y año de nacimiento información
-          demográfica. Umbra emplea la información con el fin de proporcionar el
-          mejor servicio posible, particularmente para crear una mayor
-          experiencia inmersita y mejorar nuestro servicio. Estamos altamente
-          comprometido para cumplir con el compromiso de mantener su información
-          segura.
+          Nombre, situación geográfica y año de nacimiento. Umbra emplea la
+          información con el fin de proporcionar el mejor servicio posible,
+          particularmente para crear una mayor experiencia inmersiva y mejorar
+          nuestro servicio. Estamos altamente comprometido para cumplir con el
+          compromiso de mantener su información segura.
         </p>
         <p>
           Una cookie se refiere a un fichero que es enviado con la finalidad de
           solicitar permiso para almacenarse en su ordenador, al aceptar dicho
-          fichero se crea y la cookie sirve entonces para tener información
+          fichero se crea y las cookies sirven entonces para tener información
           respecto al tráfico web, y también facilita las futuras visitas a una
           web recurrente. Nuestro sitio web no emplea las cookies para poder
           identificar las páginas que son visitadas y su frecuencia.
@@ -94,6 +93,10 @@ h3 {
   line-height: 1, 5rem;
   color: #f5f9fb;
 }
+p {
+  font-weight: 600;
+  cursor:ns-resize; 
+}
 
 .textspace {
   margin-right: 16.93rem;
@@ -113,6 +116,7 @@ h3 {
   display: flex;
   margin-left: 0.8rem;
   margin-top: 3.2rem;
+  cursor: pointer;
 }
 
 li {
@@ -139,7 +143,7 @@ h1 {
   color: #f5f9fb;
 }
 
-@media only screen and (max-width: 450px) and (min-width: 400px) {
+@media only screen and (max-width: 450px) and (min-width: 420px) {
   h1 {
     font-size: 2.2rem;
     margin-top: 3.5rem;
@@ -149,11 +153,10 @@ h1 {
     font-size: 0.875rem;
   }
   .text {
-    height: 37rem;
+    height: 38rem;
     width: 17rem;
     margin-top: 0.5rem;
     margin-left: 39.7rem;
-    
   }
   li {
     font-size: 1.3rem;
@@ -163,7 +166,30 @@ h1 {
   }
 }
 
-@media only screen and (max-width: 399px) and (min-width: 300px) {
+@media only screen and (max-width: 420px) and (min-width: 400px) {
+  h1 {
+    font-size: 2.2rem;
+    margin-top: 3.5rem;
+    margin-left: 3rem;
+  }
+  h3 {
+    font-size: 0.875rem;
+  }
+  .text {
+    height: 32.9rem;
+    width: 17rem;
+    margin-top: 0.5rem;
+    margin-left: 39.7rem;
+  }
+  li {
+    font-size: 1.3rem;
+  }
+  .navbar {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 399px) and (min-width: 340px) {
   h1 {
     font-size: 2.2rem;
     margin-top: 3.5rem;
@@ -173,7 +199,7 @@ h1 {
     font-size: 0.875rem;
   }
   .text {
-    height: 27rem;
+    height: 28rem;
     width: 16rem;
     margin-top: 1rem;
     margin-left: 41.4rem;
@@ -187,5 +213,76 @@ h1 {
   }
 }
 
+@media screen and (max-width: 339px) and (min-width: 300px) {
+  h1 {
+    font-size: 2rem;
+    margin-top: 3rem;
+    margin-left: 2rem;
+  }
+  h3 {
+    font-size: 0.875rem;
+  }
+  .text {
+    height: 23.5rem;
+    width: 14rem;
+    margin-top: 1rem;
+    margin-left: 43.6rem;
+    padding: 0rem;
+  }
+  li {
+    font-size: 1.3rem;
+  }
+  .navbar {
+    display: none;
+  }
+}
+
+@media only screen and (max-height: 820px) and (min-height: 800px) {
+  h1 {
+    font-size: 2.2rem;
+    margin-top: 3rem;
+    margin-left: 2rem;
+  }
+  h3 {
+    font-size: 0.875rem;
+  }
+  .text {
+    height: 37.5rem!important;
+    width: 16rem;
+    margin-top: 1rem;
+    margin-left: 40.9rem;
+    padding: 0rem;
+  }
+  li {
+    font-size: 1.3rem;
+  }
+  .navbar {
+    display: none;
+  }
+}
+
+@media only screen and (max-height: 840px) and (min-height: 820px) {
+  h1 {
+    font-size: 2.2rem;
+    margin-top: 3rem;
+    margin-left: 2rem;
+  }
+  h3 {
+    font-size: 0.875rem;
+  }
+  .text {
+    height: 37.5rem;
+    width: 18rem;
+    margin-top: 1rem;
+    margin-left: 38.9rem;
+    padding: 0rem;
+  }
+  li {
+    font-size: 1.3rem;
+  }
+  .navbar {
+    display: none;
+  }
+}
 
 </style>
